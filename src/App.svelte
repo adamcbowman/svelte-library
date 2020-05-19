cff567bb131dc399ec517359ce0ee10f7088a21b
Adam Bowman <50798875+adamcbowman@users.noreply.github.com>
2020-05-19 17:23:53 -0300

    Pluralsight Svelte SPA tut
    https://app.pluralsight.com/library/courses/svelte-getting-started/
    
    https://github.com/jaketrent/svelte-getting-started

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+	import { Route, Router } from 'svelte-routing';
+
+	import Library from './library/Library.svelte';
+	import Detail from './detail/Detail.svelte';
+	import Create from './create/Create.svelte';
+	
+	export let url = '';
+
+</script>
+
+<Router {url}>
+<main>
+	<Route path="/create">
+		<Create />
+	</Route>
+	<Route path="/books/:id" let:params>
+		<Detail id={params.id} />
+	</Route>
+	<Route path="/">
+	<Library />
+	</Route>
+</main>	
+</Router>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
